89be3d6da5a1ecbb0a46bbb9b5da5e8df4507079
harshsahai31 <54068061+harshsahai31@users.noreply.github.com>
2019-10-05 22:03:44 +0530

    ans 2

---
+++ Main.java
@@ -0,0 +1,13 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the String");
+        String a = sc.next();
+        System.out.println("enter the substrng index: ");
+        int b = sc.nextInt();
+        String c = a.substring(b);
+        System.out.println(c);
+    }
+}
